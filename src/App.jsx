57622dd52f7b3110c323fb3550d89f1c0abe328b
Jarthesecond2 <janis.kaldmaa@voco.ee>
2026-01-09 11:20:33 +0200

    3. Komponendi sisustamine andmetega

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,27 @@ import './App.css'
 import ExpenseItem from "./components/ExpenseItem.jsx";
 
 function App() {
+  const expenses = [ 
+  {
+    date: new Date(2024, 10, 12),
+    title: 'New book',
+    price: 30.99
+  },
+  {
+    date: new Date(2024, 10, 12),
+    title: 'New jeans',
+    price: 99.99
+  }
+]
 
   return (
+
     <>
-      <ExpenseItem />
+      <ExpenseItem data={expenses[0]} />
+      <ExpenseItem data={expenses[1]} />
     </>
       
-  )
+  );
 }
 
-export default App
+export default App;
