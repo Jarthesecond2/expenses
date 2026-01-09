62abbf520e2e085e9434b3e8a576092eae858d94
Jarthesecond2 <janis.kaldmaa@voco.ee>
2026-01-09 11:27:45 +0200

    4. Komponendi jaotamine mitmeks komponendiks

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,14 +1,14 @@
+import ExpenseDate from './ExpenseDate.jsx'
 import './ExpenseItem.css'
 
 const ExpenseItem = (props) => {
-
-    console.log(props)
-    console.log(props.data)
-
+    const day = props.data.date.toLocaleString('en-US', {day: '2-digit'});
+    const month = props.data.date.toLocaleString('en-US', {month: 'long'});
+    const year = props.data.date.getFullYear()
 
     return (
         <div className='expense-item'>
-            <div>{props.data.date.toString()} </div>
+            <ExpenseDate date={props.data.date} />
             <div className='expense-item__description'>
                 <h2>{props.data.title}</h2>
                 <div className='expense-item__price'>{props.data.price}</div>
