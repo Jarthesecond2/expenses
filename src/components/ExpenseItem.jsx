14030e530f6e79ea0bbe1cddc3a371a8c1d70e59
Jarthesecond2 <janis.kaldmaa@voco.ee>
2026-01-15 14:44:26 +0200

    Ülesanne 3 - ExpenseItem ümber kujundamine olemasoleva komponendi abil

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,19 +1,19 @@
 import ExpenseDate from './ExpenseDate.jsx'
 import './ExpenseItem.css'
-
+import Card from './Card.jsx'
 const ExpenseItem = (props) => {
     const day = props.data.date.toLocaleString('en-US', {day: '2-digit'});
     const month = props.data.date.toLocaleString('en-US', {month: 'long'});
     const year = props.data.date.getFullYear()
 
     return (
-        <div className='expense-item'>
+        <Card className='expense-item'>
             <ExpenseDate date={props.data.date} />
             <div className='expense-item__description'>
                 <h2>{props.data.title}</h2>
                 <div className='expense-item__price'>{props.data.price}</div>
             </div>
-        </div>
+        </Card>
     )
 }
 
