15a90e202f9796622ca68a2137abbf08d05cd537
Jarthesecond2 <janis.kaldmaa@voco.ee>
2026-01-15 14:47:36 +0200

    Komponentide organiseerimine oma projektis

--- src/components/Expenses/ExpenseItem.jsx
+++ src/components/Expenses/ExpenseItem.jsx
@@ -1,6 +1,6 @@
 import ExpenseDate from './ExpenseDate.jsx'
 import './ExpenseItem.css'
-import Card from './Card.jsx'
+import Card from '../UI/Card.jsx'
 const ExpenseItem = (props) => {
     const day = props.data.date.toLocaleString('en-US', {day: '2-digit'});
     const month = props.data.date.toLocaleString('en-US', {month: 'long'});
