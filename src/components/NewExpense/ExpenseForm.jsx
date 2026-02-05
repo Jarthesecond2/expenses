a7efabf408fa59974e7a4b8cbfd291cb63b0abe5
Jarthesecond2 <janis.kaldmaa@voco.ee>
2026-02-05 10:06:42 +0200

    11. Projekti täiendamine Fragment, Portals ja useRefs kasutamisega

--- src/components/NewExpense/ExpenseForm.jsx
+++ src/components/NewExpense/ExpenseForm.jsx
@@ -1,10 +1,18 @@
-import { useState, useRef } from "react";
+import { Fragment, useRef, useState } from "react";
 import "./ExpenseForm.css";
+import Error from "../UI/Error";
 
 const ExpenseForm = (props) => {
+    const [error, setError] = useState(null);
+    console.log(error);
+
     const titleInputRef = useRef();
     const amountInputRef = useRef();
     const dateInputRef = useRef();
+
+    const errorHandler = () => {
+        setError(null);
+    }
 
     const submitHandler = (event) => {
         const enteredTitle = titleInputRef.current.value;
@@ -12,6 +20,15 @@ const ExpenseForm = (props) => {
         const enteredDate = dateInputRef.current.value;
 
         event.preventDefault();
+
+        if(enteredTitle.trim().length === 0 || enteredAmount.trim().length == 0 || enteredDate.trim().length == 0 ||
+        enteredDate.trim().length == 0){
+            setError({
+                title: 'Invalid input',
+                message: 'Please enter a valid name, amount, and date (non-empty values).'
+            });
+            return;
+        }
         const expenseData = {
             title: enteredTitle,
             amount: enteredPrice,
@@ -41,50 +58,58 @@ const ExpenseForm = (props) => {
     };
 
     return (
-        <form onSubmit={submitHandler}>
-            <div className="new-expense__controls">
-                <div className="new-expense__control">
-                    <label>Title</label>
-                    <input
-                        type="text"
-                        id="title"
-                        ref={titleInputRef}
-                        onChange={titleChangeHandler}
-                        value={enteredTitle}
-                    />
-                </div>
-                <div className="new-expense__control">
-                    <label>Amount</label>
-                    <input
-                        type="number"
-                        id="amount"
-                        ref={amountInputRef}
-                        min="0.01"
-                        step="0.01"
-                        onChange={priceChangeHandler}
-                        value={enteredPrice}
-                    />
-                </div>
-                <div className="new-expense__control">
-                    <label>Date</label>
-                    <input
-                        type="date"
-                        id="date"
-                        ref={dateInputRef}
-                        min="2024-11-12"
-                        max="2026-01-31"
-                        onChange={dateChangeHandler}
-                        value={enteredDate}
-                    />
-                </div>
+        <Fragment>
+            {error && (
+                <Error
+                    title={error.title}
+                    message={error.message}
+                    onConfirm={errorHandler}
+                />
+            )}
+            <div>
+
+                <form onSubmit={submitHandler}>
+                    <div className="new-expense__controls">
+                        <div className="new-expense__control">
+                            <label>Title</label>
+                            <input
+                                type="text"
+                                id="title"
+                                ref={titleInputRef}
+                            />
+                        </div>
+                        <div className="new-expense__control">
+                            <label>Amount</label>
+                            <input
+                                type="number" min="0.01" step="0.01"
+                                id="amount"
+                                ref={amountInputRef}
+                            />
+                        </div>
+                        <div className="new-expense__control">
+                            <label>Date</label>
+                            <input
+                                type="date"
+                                id="date"
+                                ref={dateInputRef}
+                                min="2024-11-12"
+                                max="2026-01-31"
+                                onChange={dateChangeHandler}
+                                value={enteredDate}
+                            />
+                        </div>
+                    </div>
+
+                    <div className="new-expense__actions">
+                        <button type="button" onClick={props.onCancel}>
+                            Cancel
+                        </button>
+                        <button type="submit">Add Expense</button>
+                    </div>
+                </form>
             </div>
-            <div className="new-expense__actions">
-                <button type="button" onClick={props.onCancel}>
-                    Cancel
-                </button>
-                <button type="submit">Add Expense</button>
-            </div>
-        </form>
+        </Fragment>
+        
     );
 };
 
---
+++ src/components/UI/Error.jsx
@@ -0,0 +1,51 @@
+import React, {Fragment} from "react";
+import reactDOM from "react-dom";
+
+import Card from "./Card";
+import Button from "./Button";
+
+import "./Error.css";
+
+const Backdrop = (props) => {
+    return (
+        <div className="backdrop" onClick={props.onConfirm} />
+    );
+};
+
+const ModalOverlay = (props) => {
+    return (
+        <Card className="modal">
+            <header className="header">
+                <h2>{props.title}</h2>
+            </header>
+            <div className="content">
+                <p>{props.message}</p>
+            </div>
+            <footer className="footer">
+                <Button onClick={props.onConfirm}>Okay</Button>
+            </footer>
+        </Card>
+    )
+};
+
+const Error = (props) => {
+    return (
+        <Fragment>
+            {ReactDOM.createPortal(
+                <BackDrop onConfirm={props.onConfirm} />,
+                document.getElementById("backdrop-root")
+            )}
+            {ReactDOM.createPortal(
+                <ModalOverlay
+                    title={props.title}
+                    message={props.message}
+                    onConfirm={props.onConfirm}
+                />,
+                document.getElementById("overlay-root")
+                )
+                }
+        </Fragment>
+    );
+}
+
+export default Error;
